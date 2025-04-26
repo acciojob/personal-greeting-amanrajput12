95b0e41764b1e03d46e2fdc596d1f86f63673c68
Aman Rajput <rajputaman724857@gmail.com>
2025-04-26 14:23:49 +0530

    dynamic greeting

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [message,setMessage] = useState("");
   return (
     <div>
-        {/* Do not remove the main div */}
+      
+       <p>Enter your name:</p>
+       <input type="text" onChange={(e)=>setMessage(e.target.value)} />
+       {message && <p>Hello, {message}!</p>}
     </div>
   )
 }
